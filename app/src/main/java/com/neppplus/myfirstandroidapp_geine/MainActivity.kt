f642850c91a74795385f0f700a99621cb11272d8
donggyun-go <ehdrbsr43@naver.com>
2024-05-02 11:26:31 +0900

    login & regist Commit

--- app/src/main/java/com/neppplus/myfirstandroidapp_geine/MainActivity.kt
+++ app/src/main/java/com/neppplus/myfirstandroidapp_geine/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.neppplus.myfirstandroidapp_geine
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // 뷰에 대한 인셋 적용
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // 로그인 버튼, 아이디 필드, 패스워드 필드를 찾고 클릭 리스너 설정
+        val loginButton = findViewById<Button>(R.id.login)
+        val idEditText = findViewById<EditText>(R.id.idEdt)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEdt)
+
+        loginButton.setOnClickListener {
+            val inputId = idEditText.text.toString()
+            val inputPwd = passwordEditText.text.toString()
+            // 로그인 로직 실행
+            if(inputId == "admin@test.com" && inputPwd == "qwer") {
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+        // 회원가입 TextView 찾기 및 클릭 리스너 설정
+        val signupTextView = findViewById<TextView>(R.id.signupText)
+        signupTextView.setOnClickListener {
+            Toast.makeText(this, "회원가입 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+            // Intent로 다른 Activity 시작
+            val intent = Intent(this, RegistActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/neppplus/myfirstandroidapp_geine/RegistActivity.kt
+++ app/src/main/java/com/neppplus/myfirstandroidapp_geine/RegistActivity.kt
@@ -1,20 +1,25 @@
 package com.neppplus.myfirstandroidapp_geine
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class RegistActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.register_main)
+
+        val cancelBtn = findViewById<TextView>(R.id.cancelBtn)
+        cancelBtn.setOnClickListener {
+            Toast.makeText(this, "로그인 화면으로 이동합니다.", Toast.LENGTH_SHORT).show()
+            // MainActivity로 돌아가기 위한 Intent 생성
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
+            startActivity(intent)
         }
     }
 }
